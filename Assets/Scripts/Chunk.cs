6e1b54758e73a334fb1443f0f8ce59f8ccda5fa1
Jonathan <j.six@student.isartdigital.com>
2021-02-27 15:23:45 +0100

    chang chunk interface

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,15 +19,39 @@ public enum EType
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(EdgeCollider2D))]
 public class Chunk : MonoBehaviour
 {
-    private Vector2[] point;
+    private Vector2[] points;
     private IFunction function;
-    
-    public EType functionType;
-    public EInflexionType inflexionType;
-    public Rect dimension;
 
-    void Apply()
+    public void Apply(EType functionType, EInflexionType inflexionType, Rect dimension)
     {
+        switch (inflexionType)
+        {
+            case EInflexionType.ASCENDANTE:
+
+                switch (functionType)
+                {
+                    case EType.SINUSOIDE:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                break;
+            case EInflexionType.DESCANDANTE:
+                
+                switch (functionType)
+                {
+                    case EType.SINUSOIDE:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
         
+        GetComponent<EdgeCollider2D>().points = points;
     }
 }
