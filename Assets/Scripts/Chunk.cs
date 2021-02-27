702b9cad76ab3e4bf2e964d286258c18bfc2e76c
Jonathan <j.six@student.isartdigital.com>
2021-02-27 15:14:15 +0100

    Add base of project

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public enum EInflexionType
+{
+    ASCENDANTE,
+    DESCANDANTE
+}
+
+[SerializeField]
+public enum EType
+{
+    SINUSOIDE
+}
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(EdgeCollider2D))]
+public class Chunk : MonoBehaviour
+{
+    private Vector2[] point;
+    private IFunction function;
+    
+    public EType functionType;
+    public EInflexionType inflexionType;
+    public Rect dimension;
+
+    void Apply()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public enum EGenerationType
+{
+    SINUSOIDE,
+    RANDOM
+}
+
+public class WorldGenerator : MonoBehaviour
+{
+    public GameObject chunkPrefab;
+    
+    public EGenerationType generationType;
+    private Chunk[] chunks; // queue
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
