35d92555d0414cb79af5096905fce71df07187da
Six Jonathan <Six-jonathan@orange.fr>
2021-04-19 21:06:42 +0200

    rework mobile version compatibility

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Assertions;
 using Random = UnityEngine.Random;
 
 [SerializeField]
--- Assets/Scripts/Function/Function.cs
+++ Assets/Scripts/Function/Function.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq.Expressions;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Assertions;
 
