6a55d5078e4e1a2b23d9d078ec18e3c9c2c75abe
Six Jonathan <Six-jonathan@orange.fr>
2021-04-29 14:49:03 +0200

    Add seed selector

---
+++ Assets/Scripts/Menu/UI/SeedSelector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class SeedSelector : MonoBehaviour
+{
+    [SerializeField] protected Button m_minusButton;
+    [SerializeField] protected Text m_label;
+    [SerializeField] protected Button m_plusButton;
+    protected int m_index = 0;
+    
+    protected virtual void Start()
+    {
+        m_minusButton.onClick.AddListener(DecrementLabel);
+        m_plusButton.onClick.AddListener(IncrementLabel);
+        
+        m_index = PlayerPrefs.GetInt("Seed", 0);
+        m_label.text = m_index.ToString();
+    }
+    
+    public void IncrementLabel()
+    {
+        ++m_index;
+        PlayerPrefs.SetInt("Seed", m_index);
+        m_label.text = m_index.ToString();
+        Random.InitState(m_index);
+    }
+
+    public void DecrementLabel()
+    {
+        if (--m_index < 0)
+        {
+            m_index = 0;
+        }
+
+        PlayerPrefs.SetInt("Seed", m_index);
+        m_label.text = m_index.ToString();
+        Random.InitState(m_index);
+
+    }
+}
